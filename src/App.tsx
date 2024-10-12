0938481969a1320375fa4885df791443dcf15d23
Keng Hwee Tan <tankenghwee1996@gmail.com>
2024-10-12 19:27:48 +0800

    Remove outdated monster search from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { MonsterCard } from "./components/MonsterCard";
-import useMonsterSearch from "./hooks/useMonsterSearch";
 import { Container } from "@mantine/core";
 // import spellData from "./data/spells.json";
 
 function App() {
-  const { query, handleQueryChange, results } = useMonsterSearch();
-
   return (
     <Container>
       <label>Monster Name </label>
-      <input type="text" value={query} onChange={handleQueryChange} />
-      {results.map((monster) => {
-        return <MonsterCard monster={monster} key={monster.monster_id} />;
-      })}
     </Container>
   );
 }
